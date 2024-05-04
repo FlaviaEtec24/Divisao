f1a9577547302bde224010aa3b8ffdb223f3ff36
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:59:58 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+﻿double numerador, denominador, quociente;
+ 
+Console.Write("Digite o numerador......: ");
+numerador = Convert.ToDouble(Console.ReadLine());
+ 
+Console.Write("Digite o denominador....: ");
+denominador = Convert.ToDouble(Console.ReadLine());
+ 
+if (denominador == 0)
+{
+    Console.WriteLine("Não é possível dividir por zero.");
+}
+else
+{
+    quociente = numerador / denominador;
+ 
+    Console.WriteLine($"{numerador:N1} dividido por {denominador:N1} é {quociente:N1}.");
+}
+tem menu de contexto
